bee12bacefa4461947ccb74cc13b34ca5441e5d7
UppalaAravind28 <aravind.uppala2002@gmail.com>
2025-02-18 16:10:23 +0530

    server

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 const http = require("http");
 const server = http.createServer((req, res) => {
-  res.write("Hello darling");
-  res.end(" Welcome to the my SERVER");
+  res.write("Hello everyone ");
+  res.end("Hope you are fine.");
 });
 server.listen("3000", () => {
-  console.log("Happy to see u again....THIS IS UR SERVER RESPONSE");
+  console.log("I love you 3000 times");
 });
